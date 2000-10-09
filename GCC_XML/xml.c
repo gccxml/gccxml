41665e3a5066898ea20412d1fb8ded448c22ce40
Brad King <brad.king@kitware.com>
2000-10-09 15:15:24 -0400

    Added integration example and header comment.

--- GCC_XML/xml.c
+++ GCC_XML/xml.c
@@ -1,3 +1,25 @@
+/* XML output implementation for the C++ compiler.
+
+   Brad King (brad.king@kitware.com)
+   Kitware, Inc.   (www.kitware.com)
+
+This file WILL HOPEFULLY BE part of GNU CC (eventually).
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
 #include "config.h"
 #include "system.h"
 #include "sys/stat.h"
