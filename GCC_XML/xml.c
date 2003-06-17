9b48373c91d1f7dd6f163ecf458e504b7ce02aa5
Brad King <brad.king@kitware.com>
2003-06-17 14:18:43 -0400

    BUG: Fixed dumping of artificial template instantiations created by default template arguments that depend on other arguments.  Solves the _0 problem.

--- GCC_XML/xml.c
+++ GCC_XML/xml.c
@@ -155,6 +155,13 @@ static const char* xml_get_encoded_string PARAMS ((tree));
 static const char* xml_get_encoded_string_from_string PARAMS ((const char*));
 static tree xml_get_encoded_identifier_from_string PARAMS ((const char*));
 
+/* Switch to 1 to enable debugging of dump node selection.  */
+#if 0
+# define xml_add_node(xdi, node, complete)                                    \
+   (fprintf(stderr, "Adding node at line %d\n", __LINE__),                    \
+    xml_add_node(xdi, node, complete))
+#endif
+
 /* Main XML output function.  Called by parser at the end of a translation
    unit.  Walk the entire translation unit starting at the global
    namespace.  Output all declarations.  */
@@ -1456,7 +1463,11 @@ xml_add_template_decl (xml_dump_info_p xdi, tree td, int complete)
     switch (TREE_CODE (ts))
       {
       case TYPE_DECL:
-        xml_add_node (xdi, ts, complete);
+        /* Add the instantiation only if it is real.  */
+        if (!DECL_ARTIFICIAL(ts))
+          {
+          xml_add_node (xdi, ts, complete);
+          }
         break;
       default:
         /* xml_output_unimplemented (xdi, ts, 0,
@@ -1550,6 +1561,11 @@ xml_dump_tree_node (xml_dump_info_p xdi, tree n, xml_dump_node_p dn)
       xml_output_unimplemented (xdi, n, dn, 0);
     }
 }
+
+/* If we enabled xml dump debugging, undefine the macro.  */
+#if defined(xml_add_node)
+# undef xml_add_node
+#endif
 
 /* Add tree node N to those encountered.  Return its index.  */
 int
