eff5abce2fd088cd2a16c6e6f1cbea6b8652f5d6
Brad King <brad.king@kitware.com>
2003-06-26 12:51:55 -0400

    ENH: Added support for gcc 3.3.

---
+++ GCC_XML/GccInclude/3_3/gccxml_builtins.h
@@ -0,0 +1,22 @@
+#define __builtin_apply(x,y,z) ((void*)0)
+#define __builtin_nan(x) ((double)0.0)
+#define __builtin_nanf(x) ((float)0.0)
+#define __builtin_nanl(x) ((long double)0.0)
+#define __builtin_huge_val(x) ((double)0.0)
+#define __builtin_huge_valf(x) ((float)0.0)
+#define __builtin_huge_vall(x) ((long double)0.0)
+#define __builtin_apply_args(x) ((void*)0)
+#define __builtin_types_compatible_p(x,y) 0
+#define __builtin_choose_expr(x,y,z) int
+#define __builtin_constant_p(x) 0
+void __builtin_return (void *RESULT);
+void * __builtin_return_address (unsigned int LEVEL);
+void * __builtin_frame_address (unsigned int LEVEL);
+long __builtin_expect (long EXP, long C);
+void __builtin_prefetch (const void *ADDR, ...);
+double __builtin_inf (void);
+float __builtin_inff (void);
+long double __builtin_infl (void);
+double __builtin_nans (const char *str);
+float __builtin_nansf (const char *str);
+long double __builtin_nansl (const char *str);
