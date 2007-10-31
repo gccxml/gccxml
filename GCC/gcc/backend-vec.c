029a91cf2c2912d43eacfc0263b99acaab6289ce
Brad King <brad.king@kitware.com>
2007-10-31 18:09:43 -0400

    COMP: Work-around VS6 projects not using separate intermediate file directories.

---
+++ GCC/gcc/backend-vec.c
@@ -0,0 +1,5 @@
+/* This source works around VS6 projects not having separate
+   intermediate file directories.  The corresponding source is
+   supposed to be built in more than one target with different
+   preprocessor definitions.  */
+#include "vec.c"
