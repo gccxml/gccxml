b32035bb0a654ab2ad75284c8f4ad7dc716cd0a7
David Cole <david.cole@kitware.com>
2008-04-08 10:43:39 -0400

    ENH: Update site to the new machine for cdash.org

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,6 +2,6 @@ set(CTEST_PROJECT_NAME "gccxml")
 set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "public.kitware.com")
+set(CTEST_DROP_SITE "www.cdash.org")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=gccxml")
 set(CTEST_DROP_SITE_CDASH TRUE)
