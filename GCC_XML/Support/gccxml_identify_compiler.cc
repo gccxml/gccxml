fea96082fd284c89ff20ee97062152c0efbfafac
Brad King <brad.king@kitware.com>
2008-12-15 15:25:24 -0500

    BUG: Avoid running generated shell code
    The gccxml_find_flags script used to generate a file in /tmp and then
    evaluate it as shell code.  This creates a case which might allow an
    attacker to insert code to be executed when the root user runs gccxml.
    This fix removes use of /tmp and uses string processing instead of
    executing generated code.
    
    See issue #8083.

---
+++ GCC_XML/Support/gccxml_identify_compiler.cc
@@ -0,0 +1,11 @@
+#if defined(__GNUC__)
+GCCXML_SUPPORT="GCC"
+#elif defined(__sgi) && defined(_COMPILER_VERSION)
+GCCXML_SUPPORT="MIPSpro"
+#elif defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 700)
+GCCXML_SUPPORT="Intel"
+#elif defined(__SUNPRO_CC)
+GCCXML_SUPPORT="Sun"
+#else
+GCCXML_SUPPORT=""
+#endif
