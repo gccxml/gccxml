27ea5eab70b508791473115606c9cc1e89bb805f
Brad King <brad.king@kitware.com>
2009-02-05 15:59:07 -0500

    ENH: Fix 64-bit gccxml_cc1plus for VS compiler
    The file GCC/include/obstack.h defines a member of struct obstack
    differently depending on the definition of HAVE_STDDEF_H.  This makes
    sure the definition is consistent both when building libiberty source
    files and gcc source files.
    
    The file GCC/include/splay-tree.h defines an integer type that needs to
    be able to hold a pointer.  Previously it was hard-coded to the type
    "unsigned long int" which on some 64-bit compilers is only 32-bit.
    Since gengtype parses this header, the typedef must appear only once and
    must use valid integer type names (all without preprocessing).
    Therefore we must use size_t.  Technically we should include stddef.h
    too, but it seems to work as-is.

--- GCC/include/splay-tree.h
+++ GCC/include/splay-tree.h
@@ -44,8 +44,12 @@ extern "C" {
    these types, if necessary.  These types should be sufficiently wide
    that any pointer or scalar can be cast to these types, and then
    cast back, without loss of precision.  */
-typedef unsigned long int splay_tree_key;
-typedef unsigned long int splay_tree_value;
+/* BEGIN GCC-XML MODIFICATIONS 2009-02-05 */
+/* For GCC-XML this is size_t instead of unsigned long int to support
+   64-bit compilers that have a 32-bit long.  */
+typedef size_t splay_tree_key;
+typedef size_t splay_tree_value;
+/* END GCC-XML MODIFICATIONS 2009-02-05 */
 
 /* Forward declaration for a node in the tree.  */
 typedef struct splay_tree_node_s *splay_tree_node;
