cc2b67c366a243bfcecb9696d446c13eda7d3dcf
Mattias Ellert <mattias.ellert@fysast.uu.se>
2013-12-09 14:29:59 +0100

    Avoid using inline namespace not supported by GCC 4.2

--- GCC_XML/Support/GCC/4.8/bits/c++config.h
+++ GCC_XML/Support/GCC/4.8/bits/c++config.h
@@ -1,3 +1,11 @@
+#ifdef __LONG_DOUBLE_128__
+/* Avoid using inline namespace not supported by GCC 4.2 */
+#undef __LONG_DOUBLE_128__
 #include_next <bits/c++config.h>
+#define __LONG_DOUBLE_128__ 1
+#else
+#include_next <bits/c++config.h>
+#endif
+
 /* GCC 4.2 parser does not support __int128 */
 #undef _GLIBCXX_USE_INT128
